4f4b1ece1df5cf81c076198620249bcbfe72e7f7
“Keaton <kshawhan@ucsc.edu>
2024-10-15 16:57:27 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,11 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerHTML = "Cool sketch thingy"
+app.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.classList.add('custom-canvas');
+app.append(canvas);
